5f4ae4595e56910c1d3f7451997f5bb3893fba6b
Potato Hatsue <1793913507@qq.com>
2023-08-29 20:12:42 -0400

    Update

--- scripts/build-moegirl.sh
+++ scripts/build-moegirl.sh
@@ -17,7 +17,7 @@ plugin_description = Fcitx 5 Pinyin Dictionary from zh.moegirl.org.cn
 plugin_api_version = 0.1
 EOF
 
-nix develop --command runghc Main.hs
+nix run
 # nix flake needs git add
 git add -f out
 mkdir -p out/app/src/main/assets/usr/share/fcitx5/pinyin/dictionaries/
